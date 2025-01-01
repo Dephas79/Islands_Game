9d316c3f105173168b326f99367587e4b6e6046d
Dephas79 <difusisabirye@gmail.com>
2025-01-01 21:57:19 +0300

    ETS function

--- .iex.exs
+++ .iex.exs
@@ -4,3 +4,5 @@ alias IslandsEngine.Island
 alias IslandsEngine.Board
 alias IslandsEngine.Rules
 alias IslandsEngine.Game
+alias IslandsEngine.DemoProc
+alias IslandsEngine.GameSupervisor
--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -1,5 +1,6 @@
 defmodule IslandsEngine.Game do
-  use GenServer
+  use GenServer, start: {__MODULE__, :start_link, []}, restart: :transient
+
   alias IslandsEngine.{Board, Coordinate, Guesses, Island, Rules}
   @players [:player1, :player2]
 
@@ -12,10 +13,11 @@ defmodule IslandsEngine.Game do
     GenServer.start_link(__MODULE__, name, [])
   end
 
+  @timeout 60 * 60 * 24 * 1000
+
   def init(name) do
-    player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
-    player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
-    {:ok, %{player1: player1, player2: player2, rules: %Rules{}}}
+    send(self(), {:set_state, name})
+    {:ok, fresh_state(name)}
   end
 
   def add_player(game, name) when is_binary(name) do
@@ -92,10 +94,24 @@ defmodule IslandsEngine.Game do
   end
 
   defp update_player2_name(state_data, name), do: put_in(state_data.player2.name, name)
+
   defp update_rules(state_data, rules), do: %{state_data | rules: rules}
-  defp reply_success(state_data, reply), do: {:reply, reply, state_data}
+
+  defp reply_success(state_data, reply) do
+    :ets.insert(:game_state, {state_data.player1.name, state_data})
+    {:reply, reply, state_data, @timeout}
+  end
+
+  defp fresh_state(name) do
+    player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
+    player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
+    %{player1: player1, player2: player2, rules: %Rules{}}
+  end
+
   defp player_board(state_data, player), do: Map.get(state_data, player).board
+
   defp opponent(:player1), do: :player2
+
   defp opponent(:player2), do: :player1
 
   defp update_guesses(state_data, player_key, hit_or_miss, coordinate) do
@@ -115,4 +131,26 @@ defmodule IslandsEngine.Game do
 
   def guess_coordinate(game, player, row, col) when player in @players,
     do: GenServer.call(game, {:guess_coordinate, player, row, col})
+
+  def handle_info(:timeout, state_data) do
+    {:stop, {:shutdown, :timeout}, state_data}
+  end
+
+  def handle_info({:set_state, name}, _state_data) do
+    state_data =
+      case :ets.lookup(:game_state, name) do
+        [] -> fresh_state(name)
+        [{_key, state}] -> state
+      end
+
+    :ets.insert(:game_state, {name, state_data})
+    {:noreply, state_data, @timeout}
+  end
+
+  def terminate({:shutdown, :timeout}, state_data) do
+    :ets.delete(:game_state, state_data.player1.name)
+    :ok
+  end
+
+  def terminate(_reason, _state), do: :ok
 end
